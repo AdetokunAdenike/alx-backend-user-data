a4d309305bd97e5dc21f009476d9c65cafc77229
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-16 12:20:48 +0100

    Update

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,7 +21,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
+@app.errorhandler(
 def unauthorized(error) -> Tuple[str, Literal[401]]:
     """ Unauthorized handler
     """
