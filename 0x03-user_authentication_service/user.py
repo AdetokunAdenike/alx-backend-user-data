2ed10e9abd126ffd84a45b7e8e2a81af9f9b1445
AdetokunAdenike <adetokunadenike@gmail.com>
2024-11-24 14:14:31 +0100

    Update

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,16 +11,8 @@ Base = declarative_base()
 class User(Base):
     """
     User class for representing the 'users' table in the database.
-
-    Attributes:
-        id (int): The user's ID (primary key).
-        email (str): The user's email (non-nullable).
-        hashed_password (str): The user's hashed password (non-nullable).
-        session_id (str): The user's session ID (nullable).
-        reset_token (str): The user's reset token (nullable).
     """
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
